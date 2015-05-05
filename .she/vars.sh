a3fe65c13fd0af1379c7b9e9c35625d3e9cfa9ce
vsh <shekoldin@gmail.com>
2015-05-06 02:11:42 +0500

    Changed pager and less config

--- .she/vars.sh
+++ .she/vars.sh
@@ -2,6 +2,6 @@ export DEBFULLNAME='Vladimir Shekoldin'
 export DEBEMAIL='shekoldin@gmail.com'
 export EDITOR=vim
 export VIEWER=view
-export PAGER=most
-export LESS=-cex3M
+export PAGER=less
+export LESS=-icx4MR
 
